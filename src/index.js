3374f73dd62d1b15c8726caedc91e9d91e03c6a3
hoemoon <hoemoon.song@gmail.com>
2017-06-09 17:43:16 +0900

    gitignore update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import config from './config';
 import routes from './routes';
 import passport from 'passport';
