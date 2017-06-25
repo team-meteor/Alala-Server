db828cc6f1dfeab4b18c1e873efbd5efeb30214e
hoemoon <hoemoon.song@gmail.com>
2017-06-25 14:53:13 +0900

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-(cd /home/ubuntu/Alala-Server ; pm2 stop dist; git pull; npm install; npm run build; npm run start)
+(cd /home/ubuntu/Alala-Server ; pm2 stop dist; git pull; npm install; npm run prestart; npm run start)
