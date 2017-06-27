3ce81cb10aa3f5251911fc6d956f428f2d1ee3aa
hoemoon <hoemoon.song@gmail.com>
2017-06-27 18:55:12 +0900

    single image upload api updated

--- src/controller/photo.js
+++ src/controller/photo.js
@@ -9,42 +9,46 @@ import awskey from '../config/credentials'
 
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
-const sizes = [640, 320, 200, 128, 64, 40]
 AWS.config.update({
 	accessKeyId: awskey.accessKeyId, 
 	secretAccessKey: awskey.secretKey, 
 	region: 'ap-northeast-2'})
 const s3 = new AWS.S3()
+const sizes = [640, 320, 200, 128, 64, 40]
 
 export default ({ config, db }) => {
 	let api = Router()
-
 	api.post('/single', upload.single('photo'), function (req, res, next) {
-		let fileName = Date.now()
-		function sharpWithSize(size) {
-			sharp(req.file.buffer).resize(size).toBuffer((err, data, info) => {
-				return s3.putObject({
-					Bucket: 'alala-static',
-					Key: fileName + String(size) + path.extname(req.file.originalname),
-					Body: data,
-					ACL: 'public-read'
-				}, (err) => {
-					if (err) {
-						console.log(err)
-					}
-				}).promise()
-			})
-		}
-		Promise.all(sizes.map(sharpWithSize)).then(() => {
+		const fileName = Date.now()
+		let uploadCounter = 0
+		function callback() { 
 			let photo = new Photo()
 			photo.fileName = fileName
-			photo.save(function(err) {
+			photo.save((err)=> {
 				if (err) {
 					res.send(err)
 				}
-				res.json(photo._id)
+				res.send(photo._id)
 			})
-		})
+		}
+		function sharpBuffer(size) {
+			sharp(req.file.buffer).resize(size).toBuffer((err, buffer, info) => {
+				s3.putObject({
+					Bucket: 'alala-static',
+					Key: fileName + String(size) + path.extname(req.file.originalname),
+					Body: buffer,
+					ACL: 'public-read'
+				},(err, data)=> {
+					if (data) {
+						uploadCounter++
+						if (uploadCounter === sizes.length) {
+							callback()
+						}
+					}
+				})
+			})
+		}
+		sizes.forEach(sharpBuffer)
 	})
 	// 이미지 여러 개 업로드, 이미지 아이디 배열 리턴.
 	api.post('/multi', upload.array('photos', 12), function (req, res, next) {
